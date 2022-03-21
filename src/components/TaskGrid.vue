48cb07396bd6d3be9a0c68e8480a03e086ddf5f7
leofideliss <leofipp@hotmail.com>
2022-03-21 17:27:50 -0300

    Primeiro commit ex

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="task-grid"> <template>
+      <task v-for="task in tasks" :key="task.name" :task="task">
+         
+      </task>
+  </template></div>
+</template>
+<script>
+import Task from './Task.vue'
+export default {
+  components:{Task},
+  props: { tasks: { type: Array, required: true } },
+};
+</script>
+<style>
+</style>
